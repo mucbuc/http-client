fc793ffabe9705ecb37f409277e39565204650a7
markymark <smallitle@hotmail.com>
2014-01-24 10:04:13 -0800

    stubing out mac client

--- src/clients/mac.h
+++ src/clients/mac.h
@@ -1,11 +1,27 @@
 #ifndef MAC_H__Sq8JUEtgWjyMmwOAdQTfRleD5v6Zpi
 #define MAC_H__Sq8JUEtgWjyMmwOAdQTfRleD5v6Zpi
 
+#include <http-client/src/client.h> 
+
 namespace om636
 {
 	namespace http
 	{
+		template<class T, template<class> class U>
+		struct mac_client 
+		: Client< T, U > 
+		{	
+			typedef Client< T, U > base_type; 
+			using typename base_type::request_type; 
+			using typename base_type::response_type;
 
+			mac_client(); 
+			virtual ~mac_client() = default;
+			virtual response_type get(request_type);
+			virtual response_type post(request_type);
+			virtual response_type put(request_type);
+			virtual response_type del(request_type);
+		};
 	}
 }
 
