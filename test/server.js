953624cbea5179b480743eba76f2c98aef70c804
Ubuntu <ubuntu@ip-10-196-97-144.us-west-1.compute.internal>
2014-02-01 22:05:38 +0000

    test server port, feedback

--- test/server.js
+++ test/server.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-var http = require( 'http' ); 
+var http = require( 'http' )
+  , port = process.argv.length >= 3 ? parseInt( process.argv[2], 10 ) : 3000; 
+
+console.log( 'listening on port: ' + port );
 
 var server = http.createServer(function(req, res){
-	res.end( 'ok' );
+  res.end( 'ok' );
+  console.log( 'got request:', req );
 }); 
 
-server.listen(3000);
+server.listen(port);
