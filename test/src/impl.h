20648baf9d5262982f40fd87f590b6e7a90a5665
markymark <smallitle@hotmail.com>
2014-02-02 20:12:29 -0800

    xcode test

--- test/src/impl.h
+++ test/src/impl.h
@@ -1,5 +1,6 @@
 
 #include <string>
+#include <cassert>
 
 #include <http-client/src/client.h>
 #include <http-client/src/request.h>
@@ -8,6 +9,8 @@
 
 namespace impl
 {
+    static size_t passed(0);
+    
 	template<class>
 	struct test_traits
 	{
@@ -31,4 +34,92 @@ namespace impl
 	{
 		return new om636::http::mac_client< void, client_traits >(); 
 	}
+
+	template<class T>
+	void test_get(T & c, const char * url )
+	{
+	    using namespace impl;
+	    request_type req;
+	    req.url() = url;
+	    c->get( req, [&](response_type r){
+	        if (r.data() == "GET:")
+            {
+	            ++passed;
+                std::cout << "http_client::get passed" << std::endl;
+            }
+        } );
+	}
+
+	template<class T>
+	void test_post(T & c, const char * url )
+	{
+	    using namespace impl;
+	    request_type req;
+	    req.url() = url;
+	    req.method() = "POST";
+	    req.content() = "hello";
+	    c->request( req, [&](response_type r){
+	        if (r.data() == "POST:hello")
+            {
+	            ++passed;
+                std::cout << "http_client::post passed" << std::endl;
+            }
+        } );
+	}
+
+	template<class T>
+	void test_put(T & c, const char * url )
+	{
+	    using namespace impl;
+	    request_type req;
+	    req.url() = url;
+	    req.method() = "PUT";
+	    req.content() = "hello";
+	    c->request( req, [&](response_type r){
+	        if (r.data() == "PUT:hello")
+            {
+	            ++passed;
+                std::cout << "http_client::put passed" << std::endl;
+            }
+        } );
+	}
+
+	template<class T>
+	void test_delete(T & c, const char * url )
+	{
+	    using namespace impl;
+	    request_type req;
+	    req.url() = url;
+	    req.method() = "DELETE";
+	    req.content() = "hello";
+	    c->request( req, [&](response_type r){
+	        if (r.data() == "DELETE:hello")
+            {
+	            ++passed;
+                std::cout << "http_client::delete passed" << std::endl;
+            }
+        } );
+	}
+
+	void run_test(const char * arg)
+	{
+	    using namespace std;
+	    using namespace impl;
+	    
+	    // setup test
+	    unique_ptr< client_type > c( make_client() );
+		request_type req;
+	    req.url() = arg;
+	    
+	    test_get( c, arg );
+	    test_post( c, arg  );
+	    test_put( c, arg  );
+	    test_delete( c, arg  );
+	    
+	    // 
+	    sleep( 2 );
+	    assert( passed >= 4 );
+        
+        std::cout << "http-client test" << " passed" << std::endl;
+    }
 }
