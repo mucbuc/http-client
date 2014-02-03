42acbe615158274f82788c32dc4b791ffbc430db
markymark <smallitle@hotmail.com>
2014-02-02 23:08:32 -0800

    ios test

---
+++ test/ios/ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios
+//
+//  Created by mucbuc on 2/2/14.
+//  Copyright (c) 2014 markymark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
