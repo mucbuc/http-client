8c67b457e1a0b3c2bd9d31c583c15ed662408709
markymark <smallitle@hotmail.com>
2014-02-02 19:44:23 -0800

    add xcode project

---
+++ test/xcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  xcode
+//
+//  Created by mucbuc on 2/2/14.
+//  Copyright (c) 2014 markymark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
